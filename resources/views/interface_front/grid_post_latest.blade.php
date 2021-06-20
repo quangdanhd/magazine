ce8bc64dcc1f679083becb562a1727e50cdb5026
quangdanhd <quangdan.hd@gmail.com>
2021-06-20 16:35:30 +0700

    Home page: lastest

---
+++ resources/views/interface_front/grid_post_latest.blade.php
@@ -0,0 +1,29 @@
+<article class="grid-post post-10 post type-post status-publish format-standard has-post-thumbnail no-excerpt grid-post-c3 grid-post-a">
+    <div class="post-thumb">
+        <a href="@include('interface_layouts.href_news_view', ['href' => $item['url']])" class="image-link media-ratio ratio-3-2">
+            <span data-bgsrc="{{$item['image']}}" class="img bg-cover wp-post-image attachment-cheerup-768 size-cheerup-768 lazyload" role="img" data-sizes="(max-width: 370px) 100vw, 370px" title="{{$item['title']}}"></span>
+        </a>
+        <span class="cat-label cf">
+            @if(isset($obj['category']))
+                <a href="@include('interface_layouts.href_category', ['href' => $obj['category'][$item['category_id']]['url']])" class="category" rel="category">{{$obj['category'][$item['category_id']]['name']}}</a>
+            @endif
+        </span>
+    </div>
+    <div class="meta-title">
+        <div class="post-meta post-meta-c post-meta-left has-below">
+            <h2 class="is-title post-title-alt">
+                <a href="@include('interface_layouts.href_news_view', ['href' => $item['url']])">{{\Illuminate\Support\Str::limit($item['title'], $limit = 50, $end = '...')}}</a>
+            </h2>
+            <div class="below meta-below">
+                <span class="meta-item post-author">
+                    <span class="by">By</span>
+                    <span title="Posts by Shane Doe" rel="author" class="author">Admin</span>
+                </span>
+                <span class="meta-sep"></span>
+                <a href="#" class="meta-item date-link">
+                    <time class="post-date">{{date('F d, Y', strtotime($item['created_at']))}}</time>
+                </a>
+            </div>
+        </div>
+    </div>
+</article>
---
+++ resources/views/interface_front/loop_grid.blade.php
@@ -0,0 +1,26 @@
+@if($items)
+    <section class="cf block loop-grid-3">
+        <div class="block-head block-head-b">
+            <h4 class="title">Latest</h4>
+        </div>
+        <div class="block-content">
+            <div class="posts-dynamic posts-container ts-row grid count-0 has-grid-3">
+                <div class="posts-wrap">
+                    @foreach($items as $key => $value)
+                        @if($loop->index < 6)
+                            <div class="column col-4">
+                                @include('interface_front.grid_post_latest', ['item' => $value])
+                            </div>
+                        @endif
+                        @if ($loop->index >= 6)
+                            @break
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+            <div class="main-pagination load-more">
+                <a href="#" class="load-button" data-page="1"> Load More <i class="tsi tsi-repeat"></i></a>
+            </div>
+        </div>
+    </section>
+@endif

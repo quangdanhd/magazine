31dcceb6601470459404428b69eed5856f8182ef
quangdanhd <quangdan.hd@gmail.com>
2021-06-20 10:08:08 +0700

    Dieu chinh news models

--- app/Models/news.php
+++ app/Models/news.php
@@ -21,29 +21,35 @@ class news extends Model implements Authenticatable
     public static function boot()
     {
         parent::boot();
-        self::saving(function ($model) {
-            $category_id = $model->category_id;
-            DB::table('news')->where('publish', 1)->where('category_id', $category_id)->where('new_of_category', 1)->update(['new_of_category' => 0]);
-            DB::table('news')->where('publish', 1)->where('category_id', $category_id)->orderBy('id', 'desc')->take(4)->update(['new_of_category' => 1]);
-            if (!$model->id && !!$model->publish) {
-                $model->new_of_category = 1;
-            }
+        self::creating(function ($model) {
+            // Url
             $url = trim($model->title);
             $url = convert_name($url);
             $url = strtolower($url);
             // Removes special chars.
             $url = preg_replace('/[^A-Za-z0-9\s]/', '', $url);
             $url = preg_replace('/\s+/', '-', $url);
-            $id = !!$model->id ? $model->id : '';
-            if (!$id) {
-                $id_db = DB::table('news')->select('id')->orderBy('id', 'desc')->first();
-                if ($id_db) {
-                    $id = $id_db->id + 1;
-                } else {
-                    $id = 1;
-                }
+            // Check url
+            $check = DB::table('news')->select('id')->where('url', $url)->first();
+            if ($check) {
+                $url .= date('Ymd') . '-' . time();
             }
-            $model->url = $url . '-' . $id;
+            $model->url = $url;
+        });
+        self::saving(function ($model) {
+            $category_id = $model->category_id;
+            // new_of_category
+            $take = 6;
+            DB::table('news')->where('publish', 1)->where('category_id', $category_id)->where('new_of_category', 1)->update(['new_of_category' => 0]);
+            DB::table('news')->where('publish', 1)->where('category_id', $category_id)->orderBy('id', 'desc')->take($take)->update(['new_of_category' => 1]);
+            // newest
+            $take = 6;
+            DB::table('news')->where('publish', 1)->where('newest', 1)->update(['newest' => 0]);
+            DB::table('news')->where('publish', 1)->orderBy('id', 'desc')->take($take)->update(['newest' => 1]);
+            if (!$model->id && !!$model->publish) {
+                $model->new_of_category = 1;
+                $model->newest = 1;
+            }
         });
     }
 

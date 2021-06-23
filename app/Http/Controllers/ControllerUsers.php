f6558bcad47449c427630a548cd9d5ed2ef574d1
quangdanhd <quangdan.hd@gmail.com>
2021-06-23 22:34:58 +0700

    Fix cached access time when cache cleared

--- app/Http/Controllers/ControllerUsers.php
+++ app/Http/Controllers/ControllerUsers.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class ControllerUsers extends BaseController
 {
@@ -18,10 +19,15 @@ class ControllerUsers extends BaseController
         $uniqueID = session_uniqueID();
         $cache_key = 'access_times_' . $uniqueID;
         $access_times = Cache::get($cache_key);
+        // Check cached
         if ($access_times != date('Y-m-d')) {
-            access_times::create([
-                'unknown_token' => $uniqueID,
-            ]);
+            $access_today = DB::table('access_times')->select('id')->whereDate('created_at', date('Y-m-d'))->where('unknown_token', $uniqueID)->first();
+            // Check if cache cleared
+            if (!$access_today) {
+                access_times::create([
+                    'unknown_token' => $uniqueID,
+                ]);
+            }
             $seconds = 24 * 60 * 60;
             Cache::add($cache_key, date('Y-m-d'), $seconds);
         }
